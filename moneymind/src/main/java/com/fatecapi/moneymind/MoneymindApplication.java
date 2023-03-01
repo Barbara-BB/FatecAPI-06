4880863ae17bc3313da4a5ee0154486bc589cc9a
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-03-01 10:21:11 -0300

    initializing the java project using spring boot
    - basically the base project was created with the basic settings.
    
    Co-Authored-By: Pedro Willian <70727334+pedrowil12@users.noreply.github.com>

---
+++ moneymind/src/main/java/com/fatecapi/moneymind/MoneymindApplication.java
@@ -0,0 +1,13 @@
+package com.fatecapi.moneymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymindApplication.class, args);
+	}
+
+}
---
+++ moneymind/src/test/java/com/fatecapi/moneymind/MoneymindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecapi.moneymind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
