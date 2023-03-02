51a3108ea8f5cbb09b8587cb18ad2fae21824c47
Pedro Willian <70727334+pedrowil12@users.noreply.github.com>
2023-03-01 21:48:32 -0300

    database configuration and folder structure.

--- moneymind/src/main/java/com/fatecapi/moneymind/MoneymindApplication.java
+++ moneymind/src/main/java/com/fatecapi/moneymind/MoneymindApplication.java
@@ -8,6 +8,11 @@ public class MoneymindApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoneymindApplication.class, args);
+		
+		System.out.println("\n");
+		System.out.println("===================================================================================================================\n");
+		System.out.println("Aplicação inicializada na porta 8080\n");
+		System.out.println("===================================================================================================================\n");
 	}
 
 }
